ffef78cbd0a51c13b543399340e292f6d401bf83
naravey <harvey.narukami@gmail.com>
2021-08-29 17:58:24 +0700

    feat ( create note page ) : making create note page

--- components/textInput/index.jsx
+++ components/textInput/index.jsx
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
 import Input from '@components/input';
 
-function TextInput({ errorMsg, id, title, ...propsInput }) {
+function TextInput({ errorMsg, id, title, isLarge, ...propsInput }) {
+
   return (
     <div className="flex flex-col">
       <label htmlFor={id} className="text-md text-na-black mb-1">{title}</label>
       <Input
         id={id}
         error={errorMsg && true}
+        isLarge={isLarge}
         {...propsInput}
       />
       <p className="text-xs text-na-red">{errorMsg}</p>
---
+++ pages/notes/create.jsx
@@ -0,0 +1,106 @@
+import Button from '@components/button';
+import ButtonBack from '@components/buttonBack';
+import ColorStyle from '@components/colorStyle';
+import TextInput from '@components/textInput';
+import Title from '@components/title';
+import { fetchApi } from '@lib/fetching';
+import { getSession } from 'next-auth/client';
+import { useState } from 'react';
+
+function NotesPage() {
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [color, setColor] = useState('green');
+
+  const handlerTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handlerDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handlerChangeColor = (color) => {
+    setColor(color);
+  };
+
+  const handlerCreateNote = async (e) => {
+    e.preventDefault();
+
+    const body = {
+      title,
+      description, 
+      color,
+    };
+
+    const result = await fetchApi('/api/notes', {
+      method: 'POST',
+      body,
+    });
+
+    console.log(result);
+  };
+
+    return (
+      <div>
+        <div className="mb-6">
+          <ButtonBack />
+        </div>
+        <Title>Create New Note</Title>
+        <div className="grid grid-cols-12 w-full gap-4 mt-4">
+          <form className="flex flex-col col-span-7 gap-10" onSubmit={handlerCreateNote}>
+            <div className="flex flex-col gap-5">
+              <TextInput
+                type="text"
+                id="title"
+                placeholder="Title of the note"
+                title="Title"
+                value={title}
+                onChange={handlerTitle}
+                required
+              />
+              <TextInput
+                type="text"
+                id="description"
+                placeholder="Description your note..."
+                title="Description"
+                value={description}
+                onChange={handlerDescription}
+                isLarge
+                required
+              />
+            </div>
+            <Button typeButton="submit">
+              Create
+            </Button>
+          </form>
+          <div className="bg-na-light-green col-span-5 h-1/2 p-4">
+              <p>Border Color</p>
+              <ColorStyle colorActive={color} handlerChangeColor={handlerChangeColor} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
+
+export default NotesPage;
